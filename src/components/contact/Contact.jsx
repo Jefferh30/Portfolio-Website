5ab82b9683ceb48780b50634c1ddd2e638a89111
Jefferson Huera <jefferh30@gmail.com>
2023-02-27 18:28:30 -0300

    EmailJS integrated with contact form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import './contact.css'
 import {MdOutlineEmail} from 'react-icons/md'
 import {RiMessengerLine} from 'react-icons/ri'
 import {BsWhatsapp} from 'react-icons/bs'
+import emailjs from 'emailjs-com'
 
 
 const Contact = () => {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_ozs7s0d', 'template_fyrn6de', form.current, 'PKDqME9oXy-lmpBBD')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+    e.target.reset();
+  };
+
   return (
     <section id='contact'>
       <h5>Get In Touch</h5>
@@ -13,26 +27,26 @@ const Contact = () => {
       <div className="container contact_container">
         <div className="contact_options">
           <article className="contact_option">
-            <MdOutlineEmail />
+            <MdOutlineEmail className='contact_option-icon' />
             <h4>Email</h4>
             <h5>jeff@gmail.com</h5>
             <a href='mailto:jeff@gmail.com' target="_blank">Send a message</a>
           </article>
           <article className="contact_option">
-            <RiMessengerLine />
+            <RiMessengerLine className='contact_option-icon' />
             <h4>Messenger</h4>
             <h5>jeff@gmail.com</h5>
             <a href='https://m.me/profile' target="_blank">Send a message</a>
           </article>
           <article className="contact_option">
-            <BsWhatsapp />
+            <BsWhatsapp className='contact_option-icon' />
             <h4>WhatsApp</h4>
             <h5>+1234567</h5>
             <a href='https://api.whatsapp.com/send?phone+1234556' target="_blank">Send a message</a>
           </article>
         </div>
-        //End of contact options
-        <form action=''>
+        {/* End of contact options*/}
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name="name" placeholder='Your Full Name' required />
           <input type="email" name="email" placeholder='Your Email' required />
           <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
