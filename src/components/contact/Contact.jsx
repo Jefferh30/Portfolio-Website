9fb51afd46f6668d663698f922ee7573c83da585
Jefferson Huera <jefferh30@gmail.com>
2023-03-08 14:07:54 -0300

    Add message when form submited successfully

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,19 +1,21 @@
-import React, {useRef} from 'react'
+import React, {useState, useRef} from 'react'
 import './contact.css'
 import {MdOutlineEmail} from 'react-icons/md'
-import {RiMessengerLine} from 'react-icons/ri'
 import {BsWhatsapp} from 'react-icons/bs'
+import {RiMailCheckFill} from 'react-icons/ri'
 import emailjs from 'emailjs-com'
 
 
 const Contact = () => {
   const form = useRef();
+  const [submitted, setSubmitted] = useState(false);
   const sendEmail = (e) => {
     e.preventDefault();
 
     emailjs.sendForm('service_ozs7s0d', 'template_fyrn6de', form.current, 'PKDqME9oXy-lmpBBD')
       .then((result) => {
           console.log(result.text);
+          setSubmitted(true);
       }, (error) => {
           console.log(error.text);
       });
@@ -29,29 +31,31 @@ const Contact = () => {
           <article className="contact_option">
             <MdOutlineEmail className='contact_option-icon' />
             <h4>Email</h4>
-            <h5>jeff@gmail.com</h5>
-            <a href='mailto:jeff@gmail.com' target="_blank">Send a message</a>
-          </article>
-          <article className="contact_option">
-            <RiMessengerLine className='contact_option-icon' />
-            <h4>Messenger</h4>
-            <h5>jeff@gmail.com</h5>
-            <a href='https://m.me/profile' target="_blank">Send a message</a>
+            <p>jefferh30@gmail.com</p>
+            <a href='mailto:jefferh30@gmail.com' target="_blank">Send a message</a>
           </article>
           <article className="contact_option">
             <BsWhatsapp className='contact_option-icon' />
             <h4>WhatsApp</h4>
-            <h5>+1234567</h5>
-            <a href='https://api.whatsapp.com/send?phone+1234556' target="_blank">Send a message</a>
+            <p>+593993605531</p>
+            <a href='https://api.whatsapp.com/send?phone+593993605531' target="_blank">Send a message</a>
           </article>
         </div>
         {/* End of contact options*/}
-        <form ref={form} onSubmit={sendEmail}>
+
+        {submitted ? (
+        <div className='success-message'>
+            <RiMailCheckFill className='success-icon' />
+            <p>Thanks for contacting me!</p>
+        </div>
+        ) : (
+        <form ref={form} onSubmit={sendEmail} >
           <input type="text" name="name" placeholder='Your Full Name' required />
-          <input type="email" name="email" placeholder='Your Email' required />
+          <input type="email" name="email" placeholder='Your Email' required pattern="^[A-Za-z0-9](([a-zA-Z0-9,=\.!\-#|\$%\^&\*\+/\?_`\{\}~]+)*)@(?:[0-9a-zA-Z-]+\.)+[a-zA-Z]{2,9}$" />
           <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
-          <button type='submit' className='btn btn-primary'>Send Message</button>
+          <button type='submit' className='btn-contact'>Send Message</button>
         </form>
+        )}
       </div>
     </section>
   )
